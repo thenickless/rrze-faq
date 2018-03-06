dc14b1fa4dd0b231cb7333b2fa3654f1bad945ec
bmehler <bernhard.mehler@gmail.com>
2018-03-06 17:25:25 +0100

    - Backend Liste noch keine Kategorie

--- includes/faq/rrze-faq-list-table-helper.php
+++ includes/faq/rrze-faq-list-table-helper.php
@@ -50,11 +50,60 @@ Class FaqListTableHelper {
                     for($z = 0;  $z < sizeof($cat); $z++) {
                         $o[$z]['id'] = $cat[$z]['id'];
                         $o[$z]['slug'] = $cat[$z]['slug'];
+                        $o[$z]['domain'] = $v;
+                        
                     }
+                    //$stack = add_option('slugStack');
+                    //$stack[] = $o;
+                    echo '<pre>';
+                    //print_r($o);
+                    echo '</pre>';
                     
-                    $flag = 1;
+                    /*if( get_option('slugStack') === false ) {
+                        $stack = array();
+                        $stack[1] = $o;
+                        add_option('slugStack', $stack);
+                    } else {
+                        //echo 'zweites mal';
+                        $u = get_option('slugStack');
+                        for($a = 1; $a <= sizeof($u); $a++) {
+                            //echo $u[$a]['wordpress.dev'][0]['slug'];
+                            if(!array_key_exists($v, $u[$a])){
+                          echo $u[1][$v][0]['slug'];
+                            }
+                        /*foreach($u as $q => $e) {
+                            echo $e;
+                        }*/
+                      //  }
+                        /*$add = get_option('slugStack');
+                        for($a = 0; $a < sizeof($add); $a++) {
+                            foreach($)
+                            
+                        }
+                       
+                        if(!in_array($url, $add)) {
+                            array_push($server, $url);
+                            update_option('slugStack', $server);
+                        }*/
+                    //}
+                    
+                    $u = get_option('slugStack');
+                    
+                    delete_option('slugStack');
+                    
+                    $t[] = $o;
+                    
                 }
             }
+            
+              /*for($e = 0; $e < sizeof($t); $e++) {
+                       print_r($t[$e][$v]); 
+                    }*/
+                    
+                    echo '<pre>';
+                    print_r($t);
+                    echo '</pre>';
+                    $flag = 1;
 
             if($flag == 1) {
                 $clean = array_filter($response);
@@ -64,27 +113,49 @@ Class FaqListTableHelper {
                 }
 
                 $i = 1;
-                foreach($list as $k => $v) {
-                    foreach($v as $b => $c) {
+                foreach($list as $k => $b) {
+                    foreach($b as $b => $c) {
                         $item[$i]['id']         = $c['id'];
                         $item[$i]['title']      = $c['title']['rendered'];
                         $item[$i]['content']    = $c['content']['rendered'];
+                        $url = parse_url($c['guid']['rendered']);
+                        $item[$i]['domain']     = $url['host'];
+                        $host = $url['host'];
                         $output = '';
+                        $e = 0;
                         $items = $c['glossary_category'];
+                     
+                            foreach($items as $d => $z) {
+                                //if($t[$z][$v] == $host) {
+                                //echo $host;
+                                for($r = 0; $r < sizeof($t); $r++) {
+                                    //$output .= $t[$r][][0]['slug'];
+                                    
+                                    foreach($t as $s => $u) {
+                                        $output .= $u[0]['slug'];
+                                    }
+                                }
+                               
+                                //}*/
+                                 //$output .= $z;
+                            }
+                            //$output .= $z;
+                        //}
                         //$count_items = (count($items));
-                        foreach($items as $t => $e) {
-                                $output.= $e;
+                        /*foreach($items as $t => $e) {
+                            foreach($o as $d => $z) {
+                               //print_r($t);
+                            }
+                                //$output.= $e;
                           /*foreach($o as $w => $p) {
                             if ($e === $o[$w]['id']) {
                                 //echo $o[$w]['slug'];
                               $output .= $o[$w]['slug'];// . ($count_items > 1 ? ',' : '');
                             }
                           }*/
-                        }
+                        //}
                         //$out = ($count_items > 1) ? substr($output, 0, -1) : '';
                         $item[$i]['category'] = $output;
-                        $url = parse_url($c['guid']['rendered']);
-                        $item[$i]['domain']     = $url['host'];
                         $i++;
                     }
                 }
