0060687f3e368aec47e2a3a595cca6d0eb75f4ca
Benjamin Klemencic <benjamin.klemencic@fau.de>
2020-03-30 16:22:31 +0200

    2.6.6 (reorder updates in rrze-update-data.php)

--- includes/posttype/rrze-update-data.php
+++ includes/posttype/rrze-update-data.php
@@ -6,16 +6,16 @@ namespace RRZE\FAQ\Update;
 function rrze_update_old_data(){
     $old_faqs = get_posts( array( 'post_type'=> 'glossary', 'fields' => 'ids', 'posts_per_page' => -1) );
     foreach ( $old_faqs as $faq_id ){
-        // 1. update post_type
-        wp_update_post( array( 'ID' => $faq_id, 'post_type' => 'faq' ) );
-        // 2. add source
+        // 1. add source
         add_post_meta( $faq_id, 'source', 'website', TRUE );
-        // 3. update glossary_category to faq_category
         $old_categories = wp_get_post_terms( $faq_id, 'glossary_category', array( 'fields' => 'ids' ) );
+        // 2. update post_type
+        wp_update_post( array( 'ID' => $faq_id, 'post_type' => 'faq' ) );
         foreach ( $old_categories as $cat_id ){
-            wp_update_term( $cat_id, 'glossary_category', array( 'taxonomy' => 'faq_category'));
-            // 4. add source to faq_category
+            // 3. add source to faq_category
             add_term_meta( $cat_id, 'source', 'website', TRUE );
+            // 4. update glossary_category to faq_category
+            wp_update_term( $cat_id, 'glossary_category', array( 'taxonomy' => 'faq_category'));
         }
     }
 }
