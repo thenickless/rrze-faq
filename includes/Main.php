af4ed2581dfbf2e34a109fa10663336eada725d1
Benjamin Klemencic <benjamin.klemencic@fau.de>
2020-03-31 16:12:49 +0200

    2.6.7 ("update old data" is outsourced to a new plugin from now on )

--- includes/Main.php
+++ includes/Main.php
@@ -66,8 +66,6 @@ class Main {
         include_once( __DIR__ . '/posttype/rrze-faq-helper.php' );
         include_once( __DIR__ . '/REST-API/rrze-faq-rest-filter.php' );
 
-        include_once( __DIR__ . '/posttype/rrze-update-data.php' );
-
         // include_once( __DIR__ . '/REST-API/rrze-faq-posttype-rest.php' );
         // include_once( __DIR__ . '/faq/rrze-faq-list-table-helper.php' );
         // include_once( __DIR__ . '/faq/rrze-faq-list-table.php' );
