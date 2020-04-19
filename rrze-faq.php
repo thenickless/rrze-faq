889e152efa6320ad46c5be4a1ec00f195abd823a
Benjamin Klemencic <benjamin.klemencic@fau.de>
2020-04-19 11:31:16 +0200

    4.8.2 (translations)

--- rrze-faq.php
+++ rrze-faq.php
@@ -4,7 +4,7 @@
 Plugin Name:     RRZE FAQ
 Plugin URI:      https://gitlab.rrze.fau.de/rrze-webteam/rrze-faq
 Description:       
-Version:         4.8.1
+Version:         4.8.2
 Author:          RRZE Webteam
 Author URI:      https://blogs.fau.de/webworking/
 License:         GNU General Public License v2
