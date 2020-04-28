b3a482ad82b4e301fcf3273e6759c01e190c15a0
Benjamin Klemencic <benjamin.klemencic@fau.de>
2020-04-28 15:51:43 +0200

    4.13.0 (autosync)

--- includes/Main.php
+++ includes/Main.php
@@ -4,10 +4,10 @@ namespace RRZE\FAQ;
 
 defined('ABSPATH') || exit;
 
+use function RRZE\FAQ\Config\logIt;
 use function RRZE\FAQ\Config\deleteLogfile;
 use RRZE\FAQ\API;
 use RRZE\FAQ\CPT;
-use RRZE\FAQ\FAQCronjob;
 use RRZE\FAQ\Layout;
 use RRZE\FAQ\RESTAPI;
 use RRZE\FAQ\Settings;
@@ -42,6 +42,8 @@ class Main {
         // Actions: sync, add domain, delete domain, delete logfile
         add_action( 'update_option_rrze-faq', [$this, 'checkSync'] );
         add_filter( 'pre_update_option_rrze-faq',  [$this, 'switchTask'], 10, 1 );
+        // Auto-Sync
+        add_action( 'rrze_faq_auto_update', [$this, 'runFAQCronjob'], 10 );
 
         $cpt = new CPT(); 
 
@@ -115,8 +117,7 @@ class Main {
                         $activateCronjob = TRUE;
                     } 
                 }
-                $mycronjob = new FAQCronjob();
-                $mycronjob->setFAQCronjob( $activateCronjob );
+                $this->setFAQCronjob( $activateCronjob );
                 $options['timestamp'] = time();
             break;
             case 'del':
@@ -139,5 +140,65 @@ class Main {
             $sync->doSync( 'manual' );
         }
     }
+
+    public function runFAQCronjob() {
+        logIt( date("Y-m-d H:i:s") . ' in runFAQCronjob() START' );
+
+        // sync hourly for testing
+
+        // // Wochentags, tagsüber 8-18 Uhr alle 3 Stunden, danach und am Wochenende: Alle 6 Stunden
+        // $sync = [
+        //     'workdays' => [ 2, 8, 11, 14, 17, 20 ],
+        //     'weekend' => [ 6, 12, 18, 0 ] 
+        // ];
+
+        // date_default_timezone_set('Europe/Berlin');
+        // $today = getdate();
+        // $weekday = $today["wday"]; // 0=sunday
+        // $hour = $today["hours"]; // 0 - 23
+
+        // if ( $weekday > 0 && $weekday < 6 ){
+        //     if ( in_array( $hour, $sync["workdays"] ) ) {
+                $sync = new Sync();
+                $sync->doSync( 'automatic' );
+        //             }
+        // } else {
+        //     if ( in_array( $hour, $sync["weekend"] ) ) {
+        //         $sync = new Sync();
+        //         $sync->doSync( 'automatic' );
+        //     }
+        // }
+        logIt( date("Y-m-d H:i:s") . ' in runFAQCronjob() END' );
+    }
+
+    public function setFAQCronjob( $activate ) {
+        date_default_timezone_set( 'Europe/Berlin' );
+        logIt( date("Y-m-d H:i:s") . ' in setFAQCronjob() START $activate = ' . ( $activate ? 'TRUE' : 'FALSE') );
+
+        if ( !$activate ) {
+            if ( wp_next_scheduled( 'rrze_faq_auto_update' ) ) {
+                wp_clear_scheduled_hook( 'rrze_faq_auto_update' );
+            }
+            return;
+        }
+
+        //Use wp_next_scheduled to check if the event is already scheduled*/
+        if( !wp_next_scheduled( 'rrze_faq_auto_update' )) {
+            wp_schedule_event( time() + 120, 'hourly', 'rrze_faq_auto_update' );
+        }
+
+        $timestamp = wp_next_scheduled( 'rrze_faq_auto_update' );
+        if ($timestamp) {
+            $message = __( 'Settings saved', 'rrze-faq' )
+                . '<br />'
+                . __( 'Next automatically synchronization:', 'rrze-faq' ) . ' '
+                // . get_date_from_gmt( date( 'Y-m-d H:i:s', $timestamp ), 'd.m.Y - H:i' );
+                . date( 'd.m.Y H:i:s', $timestamp );
+            add_settings_error( 'AutoSyncComplete', 'autosynccomplete', $message , 'updated' );
+            settings_errors();
+        }
+        logIt( date("Y-m-d H:i:s") . ' in setFAQCronjob() END' );
+    }
+
 
 }
--- rrze-faq.php
+++ rrze-faq.php
@@ -4,7 +4,7 @@
 Plugin Name:     RRZE FAQ
 Plugin URI:      https://gitlab.rrze.fau.de/rrze-webteam/rrze-faq
 Description:       
-Version:         4.12.2
+Version:         4.13.0
 Author:          RRZE Webteam
 Author URI:      https://blogs.fau.de/webworking/
 License:         GNU General Public License v2
@@ -111,7 +111,6 @@ function deactivation() {
 
     // delete_option(Options::get_option_name());
     wp_clear_scheduled_hook( 'rrze_faq_auto_update' );
-    wp_clear_scheduled_hook( 'rrze_faq_auto_sync' );
 }
 
 /**
