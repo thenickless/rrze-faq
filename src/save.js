ecb3ea988dd00f5d8d8e19c5f2e70b5667d75c2f
Benjamin Klemencic <benjamin.klemencic@fau.de>
2024-02-11 16:41:12 +0100

    5.0.2 (cleanup save.js)

--- src/save.js
+++ src/save.js
@@ -1,28 +1,4 @@
-import { useBlockProps } from '@wordpress/block-editor';
-
-export default function save( { attributes } ) {
-    // console.log('in save.js');
-
-    const { id, defaultID, showFaqID } = attributes;
-    // console.log('in save.js id = ' + id);
-    // console.log('in save.js defaultID = ' + defaultID);
-    // console.log('in save.js showID = ' + showFaqID);
-
-    if ( ! defaultID ) {
-        return null;
-    }
-
-    let displayFAQ;
-
-    if ( id ) {
-        displayFAQ = id;
-    } else {
-        displayFAQ = defaultID;
-    }
-
-    // console.log('in save.js displayFAQ = ' + displayFAQ);
-
-    return (
-       <p { ...useBlockProps.save() }>von save.js { displayFAQ }</p>
-    );
+//It's a dynamic block
+export default function save() {
+	return null;
 }
