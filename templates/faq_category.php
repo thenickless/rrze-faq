14b276c627536a2f25a09e69bd15f015613327d5
Benjamin Klemencic <benjamin.klemencic@fau.de>
2024-10-09 10:10:17 +0200

    5.3.3 (fixes #120 )

--- templates/faq_category.php
+++ templates/faq_category.php
@@ -3,7 +3,7 @@
 Template Name: Custom Taxonomy faq_category Template
 */
 
-include_once('template-parts/head.php');
+include_once('template-parts/archive_head.php');
 $taxonomy = 'faq_category';
 include_once('template-parts/faq_taxonomy.php');
 include_once('template-parts/foot.php');
