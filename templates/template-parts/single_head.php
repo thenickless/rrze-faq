b5b6beb2266340790bd68ca4dfbbd5ba61cde897
Benjamin Klemencic <benjamin.klemencic@fau.de>
2025-05-06 11:10:40 +0200

    5.3.14 (closes #131 )

--- templates/template-parts/single_head.php
+++ templates/template-parts/single_head.php
@@ -7,39 +7,50 @@
  * @since FAU 1.0
  */
 
-use RRZE\FAQ\Layout;
+// 5.3.14 - Removed theme-specific templates for switches. Current and upcoming theme versions no longer require them. See https://github.com/RRZE-Webteam/rrze-faq/issues/131
 
-$thisThemeGroup = Layout::getThemeGroup();
+// use RRZE\FAQ\Layout;
+
+// $thisThemeGroup = Layout::getThemeGroup();
 
 get_header();
 
-if ($thisThemeGroup == 'fauthemes') {
-    $currentTheme = wp_get_theme();		
-    $vers = esc_attr($currentTheme->get('Version'));  // Escape der Theme-Version
+// if ($thisThemeGroup == 'fauthemes') {
+//     $currentTheme = wp_get_theme();		
+//     $vers = esc_attr($currentTheme->get('Version'));  // Escape der Theme-Version
     
-    if (version_compare($vers, "2.3", '<')) {      
-        get_template_part('template-parts/hero', 'small'); 
-    }
+//     if (version_compare($vers, "2.3", '<')) {      
+//         get_template_part('template-parts/hero', 'small'); 
+//     }
 ?>
 
+<!--
     <div id="content">
         <div class="content-container">
             <div class="post-row">
                 <main class="col-xs-12">
-                    <h1 class="screen-reader-text"><?php echo esc_html__('Index', 'fau'); ?></h1>  <!-- Escape der Übersetzung -->
+                    <h1 class="screen-reader-text">
+                        <?php // echo esc_html__('Index', 'fau'); ?>  
+                    </h1>
+-->
 
-<?php } elseif ($thisThemeGroup == 'rrzethemes') { ?>
+<?php 
+// } elseif ($thisThemeGroup == 'rrzethemes') { ?>
 
-    <?php if (!is_front_page()) { ?>
+    <?php // if (!is_front_page()) { ?>
+<!--
         <div id="sidebar" class="sidebar">
-            <?php get_sidebar('page'); ?>
-        </div><!-- .sidebar -->
-    <?php } ?>
+            <?php // get_sidebar('page'); ?>
+        </div>
+-->
+    <?php // } ?>
 
+<!--
     <div id="primary" class="content-area">
         <main id="main" class="site-main">
+-->
 
-<?php } else { ?>
+<?php // } else { ?>
 
     <div id="sidebar" class="sidebar">
         <?php get_sidebar(); ?>
@@ -47,4 +58,4 @@ if ($thisThemeGroup == 'fauthemes') {
     <div id="primary" class="content-area">
         <main id="main" class="site-main">
 
-<?php }
+<?php // } ?>
