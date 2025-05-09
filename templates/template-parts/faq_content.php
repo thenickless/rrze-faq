4891d96d4558dbd505bd01b5d4d4d926872ca990
Benjamin Klemencic <benjamin.klemencic@fau.de>
2025-05-09 19:34:50 +0200

    5.3.17 (closes #136 )

--- templates/template-parts/faq_content.php
+++ templates/template-parts/faq_content.php
@@ -2,7 +2,6 @@
 /**
  * This is part of the templates for displaying the FAQ
  *
- *
  * @package WordPress
  * @subpackage FAU
  * @since FAU 1.0
@@ -12,24 +11,29 @@ namespace RRZE\FAQ;
 
 use RRZE\FAQ\Layout;
 
-echo '<div id="post-' . esc_attr(get_the_ID()) . '" class="' . esc_attr(implode(' ', get_post_class())) .'">';
-
-?>
-
-<h2 class="glossary-title" itemprop="title"><?php echo esc_html(get_the_title()); ?></h2>
-
-<?php 
-
 $postID = get_the_ID();
-$cats = wp_kses_post(Layout::getTermLinks( $postID, 'faq_category' ));
-$tags = wp_kses_post(Layout::getTermLinks( $postID, 'faq_tag' ));            
+$random = wp_rand(); // In case there are multiple FAQs on the same page
+$header_id = 'header-' . $postID . '-' . $random;
 
-$details = '<article class="news-details"><p class="meta-footer">'
-. ( $cats ? '<span class="post-meta-categories"> ' . esc_html__( 'Categories', 'rrze-faq' ) . ': ' . $cats . '</span>' : '' )
-. ( $tags ? '<span class="post-meta-tags"> ' . esc_html__( 'Tags', 'rrze-faq' ) . ': ' . $tags . '</span>' : '' )
-. '</p></article>';
+$cats = Layout::getTermLinks($postID, 'faq_category');
+$tags = Layout::getTermLinks($postID, 'faq_tag');
 
-the_content(); 
-echo wp_kses_post($details);
+echo '<div class="rrze-faq" aria-labeledby="' . esc_attr($header_id) . '">';
+echo '<article>';
+echo '<header>';
+echo '<h1 id="' . esc_attr($header_id) . '">' . esc_html(get_the_title()) . '</h1>';
+echo '</header>';
 
+the_content();
+
+echo '<footer><p class="meta-footer">';
+if ($cats) {
+    echo '<span class="post-meta-categories">' . esc_html__('Categories', 'rrze-faq') . ': ' . wp_kses_post($cats) . '</span> ';
+}
+if ($tags) {
+    echo '<span class="post-meta-tags">' . esc_html__('Tags', 'rrze-faq') . ': ' . wp_kses_post($tags) . '</span>';
+}
+echo '</p></footer>';
+
+echo '</article>';
 echo '</div>';
